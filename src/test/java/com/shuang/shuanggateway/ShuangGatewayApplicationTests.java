e2323ba1220c611493e11e31a9f7cb51bb8ea661
shuanglaoshiailaopo <26423827@qq.com>
2024-09-07 11:08:43 +0800

    完成-

---
+++ src/test/java/com/shuang/shuanggateway/ShuangGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuang.shuanggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuangGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
